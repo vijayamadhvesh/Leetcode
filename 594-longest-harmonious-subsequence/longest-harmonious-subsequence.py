6e17d699e0d104dfe05c44bc6430ed000e8c0296
vijayamadhvesh <49319278+vijayamadhvesh@users.noreply.github.com>
2025-06-30 11:40:56 +0530

    Time: 31 ms (56.05%) | Memory: 19.1 MB (46.65%) - LeetSync

--- 594-longest-harmonious-subsequence/longest-harmonious-subsequence.py
+++ 594-longest-harmonious-subsequence/longest-harmonious-subsequence.py
@@ -2,8 +2,10 @@ class Solution:
     def findLHS(self, nums: List[int]) -> int:
         counter = Counter(nums)
         max_length = 0
-        visit = set()
-        for num in nums:
-            if num-1 in counter and num not in visit:
-                max_length = max(max_length, counter[num] + counter[num-1])
+
+        unique_list = sorted(counter)
+
+        for i in range(1, len(unique_list)):
+            if unique_list[i] - unique_list[i-1] == 1:
+                max_length = max(max_length, counter[unique_list[i]] + counter[unique_list[i-1]])
         return max_length
