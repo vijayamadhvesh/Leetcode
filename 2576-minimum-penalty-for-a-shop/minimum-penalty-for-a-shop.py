c55bcf1f1afbe5f223213c098afed7eb18d85ddd
vijayamadhvesh <49319278+vijayamadhvesh@users.noreply.github.com>
2025-07-02 18:36:37 +0530

    Time: 42 ms (96.09%) | Memory: 18.2 MB (67.38%) - LeetSync

--- 2576-minimum-penalty-for-a-shop/minimum-penalty-for-a-shop.py
+++ 2576-minimum-penalty-for-a-shop/minimum-penalty-for-a-shop.py
@@ -1,11 +1,17 @@
 class Solution:
     def bestClosingTime(self, customers: str) -> int:
-        max_score = score = 0
-        best_hour = -1
+        
+        profit, max_profit  = 0, 0
+        best_time = -1
 
-        for i, c in enumerate(customers):
-            score += 1 if c == 'Y' else -1
-            if score > max_score:
-                max_score, best_hour = score, i
-                
-        return best_hour + 1
+        for i, visit in enumerate(customers):
+            if visit == 'Y':
+                profit += 1
+            else:
+                profit -= 1
+            
+            if profit > max_profit:
+                max_profit = profit
+                best_time = i
+        
+        return best_time + 1 # as it is best to close shop after the best profitable day
