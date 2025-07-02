48b9188eff466883d712275b7fff3f89bc5ced67
vijayamadhvesh <49319278+vijayamadhvesh@users.noreply.github.com>
2025-07-02 17:32:40 +0530

    Time: 174 ms (5.02%) | Memory: 32.3 MB (5.21%) - LeetSync

--- 2145-grid-game/grid-game.py
+++ 2145-grid-game/grid-game.py
@@ -1,11 +1,21 @@
 class Solution:
     def gridGame(self, grid: List[List[int]]) -> int:
-        ans = 0
-        ts = sum(grid[0][1::])
-        v = 0
-        ans = ts
-        for i in range(len(grid[0])-1):
-            v += grid[1][i]
-            ts -= grid[0][i+1]
-            ans = min(ans, max(v, ts))
-        return ans
+        n = len(grid[0])
+        prefix_top = grid[0].copy()
+        prefix_bottom = grid[1].copy()
+
+        for i in range(1, n):
+            prefix_top[i] += prefix_top[i-1]
+            prefix_bottom[i] += prefix_bottom[i-1]
+
+        print(prefix_top)
+        print(prefix_bottom)
+        res = float('inf')
+
+        for i in range(n):
+            top_after = prefix_top[-1] - prefix_top[i]  # grid[0][i+1:]
+            bottom_before = prefix_bottom[i-1] if i>0 else 0             # grid[1][:i]
+            
+            res = min(res, max(top_after, bottom_before))
+
+        return res
