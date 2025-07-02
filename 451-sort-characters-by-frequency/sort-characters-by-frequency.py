41309c6c17ab0e9a92c0d245ce23aafd81a79dba
vijayamadhvesh <49319278+vijayamadhvesh@users.noreply.github.com>
2025-07-02 13:04:38 +0530

    Time: 11 ms (50.12%) | Memory: 19.1 MB (46.89%) - LeetSync

--- 451-sort-characters-by-frequency/sort-characters-by-frequency.py
+++ 451-sort-characters-by-frequency/sort-characters-by-frequency.py
@@ -1,9 +1,23 @@
 class Solution:
     def frequencySort(self, s: str) -> str:
-        freq_map = Counter(s)
-        sorted_chars = sorted(freq_map.items(), key=lambda x: -x[1])
-        print(sorted_chars)
+         # Allocate frequency array for ASCII range from 'A'(65) to 'z'(122)
+        char_freq = [0] * (ord('z') - ord('0') + 1)  # size = 58
+
+        for char in s:
+            char_freq[ord(char) - ord('0')] += 1
+
+        # Build (char, frequency) pairs
+        freq_pairs = []
+        for i, freq in enumerate(char_freq):
+            if freq > 0:
+                char = chr(ord('0') + i)
+                freq_pairs.append((freq, char))
+
+        # Sort by decreasing frequency
+        freq_pairs.sort(reverse=True)
+        # Build result string
         res = ''
-        for char, freq in sorted_chars:
+        for freq, char in freq_pairs:
             res += char * freq
+
         return res
