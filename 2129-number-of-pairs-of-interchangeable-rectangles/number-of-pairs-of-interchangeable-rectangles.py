6dc0f2924b94febf30cde63a1550d8386d1f1cfa
vijayamadhvesh <49319278+vijayamadhvesh@users.noreply.github.com>
2025-07-04 18:33:40 +0530

    Time: 91 ms (56.74%) | Memory: 63.1 MB (69.97%) - LeetSync

--- 2129-number-of-pairs-of-interchangeable-rectangles/number-of-pairs-of-interchangeable-rectangles.py
+++ 2129-number-of-pairs-of-interchangeable-rectangles/number-of-pairs-of-interchangeable-rectangles.py
@@ -1,11 +1,10 @@
 class Solution:
     def interchangeableRectangles(self, rectangles: List[List[int]]) -> int:
         res = 0
-        temp =[]
+        freq = Counter()
         for w,h in rectangles:
-            temp.append(w/h)
+            freq[w/h]+=1
 
-        freq = Counter(temp)
         for val in freq.values():
             if val > 1:
                 cal = val*(val -1) //2
