8870f7c53b41d5887e9892464a016b4cc0d791c7
vijayamadhvesh <49319278+vijayamadhvesh@users.noreply.github.com>
2025-07-07 09:38:54 +0530

    Time: 97 ms (97.77%) | Memory: 54.5 MB (13.71%) - LeetSync

--- 1478-maximum-number-of-events-that-can-be-attended/maximum-number-of-events-that-can-be-attended.py
+++ 1478-maximum-number-of-events-that-can-be-attended/maximum-number-of-events-that-can-be-attended.py
@@ -1,26 +1,19 @@
 class Solution:
     def maxEvents(self, events: List[List[int]]) -> int:
-        events.sort()
-        
-        min_heap = []
-        day = 0
-        index = 0
-        n = len(events)
-        result = 0
-        
-        while min_heap or index < n:
-            if not min_heap:
-                day = events[index][0]
-            while index < n and events[index][0] <= day:
-                heapq.heappush(min_heap, events[index][1])
-                index += 1
-            heapq.heappop(min_heap)
-            result += 1
-            day += 1
-            
-            while min_heap and min_heap[0] < day:
-                heapq.heappop(min_heap)
-        
-        return result
+        events.sort(key=lambda e: e[1])
 
-        
+        mx = events[-1][1]
+        fa = list(range(mx + 2))
+
+        def find(x: int) -> int:
+            if fa[x] != x:
+                fa[x] = find(fa[x])
+            return fa[x]
+
+        ans = 0
+        for start_day, end_day in events:
+            x = find(start_day)
+            if x <= end_day:
+                ans += 1
+                fa[x] = x + 1
+        return ans
