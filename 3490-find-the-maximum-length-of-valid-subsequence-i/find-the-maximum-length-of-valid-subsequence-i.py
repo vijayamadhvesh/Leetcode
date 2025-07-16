017c365cd1e0595ff9864fe32a33fc42543c585a
vijayamadhvesh <49319278+vijayamadhvesh@users.noreply.github.com>
2025-07-16 14:44:53 +0530

    Time: 44 ms (89.15%) | Memory: 39.1 MB (72.09%) - LeetSync

--- 3490-find-the-maximum-length-of-valid-subsequence-i/find-the-maximum-length-of-valid-subsequence-i.py
+++ 3490-find-the-maximum-length-of-valid-subsequence-i/find-the-maximum-length-of-valid-subsequence-i.py
@@ -1,19 +1,17 @@
 class Solution:
     def maximumLength(self, nums: List[int]) -> int:
-        count_even = 0
-        count_odd = 0
-        for num in nums:
-            if num % 2 == 0:
-                count_even += 1
+        n=len(nums)
+        odd, even, alternate=0, 0, 0
+        flag=-1
+        for i in range(n):
+            if nums[i]%2==0:
+                even+=1
+                if flag!=1:
+                    alternate+=1
+                    flag=1
             else:
-                count_odd += 1
-        
-        even_dp = 0
-        odd_dp = 0
-        for num in nums:
-            if num % 2 == 0:
-                even_dp = max(even_dp, odd_dp + 1)
-            else:
-                odd_dp = max(odd_dp, even_dp + 1)
-        
-        return max(count_even, count_odd, even_dp, odd_dp)
+                odd+=1
+                if flag!=0:
+                    alternate+=1
+                    flag=0
+        return max(even, odd, alternate)
